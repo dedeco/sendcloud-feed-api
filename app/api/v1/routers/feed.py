a1bf83eaf3b9fe1ad144148d4f5c68d0a89b1e53
André de Sousa Araújo <dedeco@gmail.com>
2021-05-19 01:11:08 -0300

    Removing some console warnings

--- app/api/v1/routers/feed.py
+++ app/api/v1/routers/feed.py
@@ -50,7 +50,6 @@ async def update_feed(id: str, feed: UpdateFeedModel = Body(...)):
         update_result = await db["feeds"].update_one({"_id": id}, {"$set": feed})
         if update_result.modified_count == 1:
             celery_app.send_task("app.workers.entries.tasks.update_feed", args=[id])
-            print('enviei')
             if (updated_feed := await db["feeds"].find_one({"_id": id})) is not None:
                 if updated_feed.get('last_updated'):
                     celery_app.send_task("app.workers.entries.tasks.update_feed_item", args=[updated_feed.get("url")])
